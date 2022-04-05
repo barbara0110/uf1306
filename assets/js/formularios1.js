0e4a87cfdbeba0d57e4f2a425dd0dddf2e885521
barbara0110 <barbararodrilo@gmail.com>
2022-04-05 13:09:32 +0200

    formularios2.js

--- assets/js/formularios1.js
+++ assets/js/formularios1.js
@@ -48,11 +48,16 @@ function validarFormulario() {
     var valido = true;
     var expRegNombre = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,50}$/;
     var expRegEmail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    // Indica que el primer nº debe ser 6,7,8 ó 9 y que los 8 siguientes entre el 0 y el 9
+    var expRegTfno = /^[6-9]{1}[0-9]{8}$/;
 
     // Objetos document HTML
     var formulario = document.getElementById("form")
     var nombre = document.getElementById("name");
     var email = document.getElementById("email");
+    var mensaje = document.getElementById("mensaje");
+    var edad = document.getElementById("edad");
+    var tfno = document.getElementById("tfno");
 
     // Validar con JavaScript que el campo “nombre” no esté vacío
 
@@ -86,7 +91,7 @@ function validarFormulario() {
 
     // Validar con JavaScript que el campo “mail” cumpla el patrón declarado
 
-    else if (!expRegEmail.exec(nombre.email)) {
+    else if (!expRegEmail.exec(email.value)) {
         // Si no es válido mostrará el mensaje “El campo email sólo acepta letras, números, @...”
         alert("El campo email sólo acepta letras, números, los símbolos _ . -  y precisa una @ ");
         // y se pondrá el foco en el campo “email”
@@ -94,6 +99,26 @@ function validarFormulario() {
         valido = false;
     }
 
+    else if (mensaje.length>255) {
+        alert("El testo ha excedido el límite de 255 caracteres");
+        mensaje.focus();
+        valido = false;
+    }
+
+    // Edad menor de 18 o edad mayor que 120 no se admite
+    else if (edad.value <18 || edad.value >120) {
+        alert("La edad debe estar comprendida entre 18 y 120 años");
+        edad.focus();
+        valido.false;
+
+    }
+
+    else if(!expRegTfno.exec(tfno.value)){
+        alert("El número de teléfono no es válido")
+        edad.focus();
+        valido.false;
+
+    }
     
 
     // Si todos los campos son válidos
